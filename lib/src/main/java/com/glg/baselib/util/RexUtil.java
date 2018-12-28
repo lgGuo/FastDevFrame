d455997caaed82470697081fa204c0828f03e87f
郭林广 <guoguanglin@tyche.net.cn>
2018-12-28 11:45:49 +0800

    正则工具类

---
+++ lib/src/main/java/com/glg/baselib/util/RexUtil.java
@@ -0,0 +1,44 @@
+package com.glg.baselib.util;
+
+public class RexUtil {
+
+    /**
+     * 数字
+     */
+    public static final String NUM_REX="^[0-9]*$";
+
+    /**
+     * 非负整数
+     */
+    public static final String POSITIVE_INTEGER="^[1-9]\\d*|0$";
+
+
+
+    /**
+     * 汉字
+     */
+    public static final String CHINESE="^[\\u4e00-\\u9fa5]{0,}$";
+
+
+    /**
+     * 钱
+     */
+    public static final String MONEY="^([0-9]+|[0-9]{1,3}(,[0-9]{3})*)(.[0-9]{1,2})?$";
+
+
+    /**
+     * 空白行
+     */
+    public static final String EMPTY_LINE="\\n\\s*\\r";
+
+
+
+    /**
+     * 空格
+     */
+    public static final String SPACE="\\s*";
+
+
+
+
+}
